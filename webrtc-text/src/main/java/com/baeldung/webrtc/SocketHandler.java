16968aa9cb9147fa6f957ef059fc7037542588df
HenrikHummelgren <henrik.hummelgren@gmail.com>
2020-09-23 22:05:25 +0200

    Making webrtc work with more than two clients.

--- webrtc-text/src/main/java/com/baeldung/webrtc/SocketHandler.java
+++ webrtc-text/src/main/java/com/baeldung/webrtc/SocketHandler.java
@@ -1,25 +1,68 @@
 package com.baeldung.webrtc;
 
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+
 
 @Component
 public class SocketHandler extends TextWebSocketHandler {
 
     List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+    HashMap<String, String> sessionIdHashMap = new HashMap<String, String>();
 
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) throws InterruptedException, IOException {
 
+        String event = "";
+        String fromName = "";
+        String toName = "";
+
+        try {
+            JSONParser parser = new JSONParser();
+            JSONObject jsonObject = (JSONObject) parser.parse(message.getPayload());
+            event = (String) jsonObject.get("event");
+            fromName = (String) jsonObject.get("fromName");
+            toName = (String) jsonObject.get("toName");
+        }
+        catch (Exception e) {
+            System.out.println("Reading failed: " + e);
+        }
+        System.out.println("event: [" + event + "]");
+        System.out.println("fromName: " + fromName);
+        System.out.println("toName: " + toName);
+
+        if ("ping".equals(event)) {
+            sessionIdHashMap.put(fromName, session.getId());
+            return;
+        }
+
         for (WebSocketSession webSocketSession : sessions) {
-            if (webSocketSession.isOpen() && !session.getId().equals(webSocketSession.getId())) {
+
+            System.out.println("webSocketSession: " + webSocketSession);
+            System.out.println("sessionIdHashMap.get(toName): " + sessionIdHashMap.get(toName));
+
+//            if (webSocketSession.isOpen() && !session.getId().equals(webSocketSession.getId())) {
+            if (webSocketSession.isOpen() && webSocketSession.getId().equals(sessionIdHashMap.get(toName))) {
+
+//                if (message.getPayload().contains("\"event\":\"offer\"")) {
+//                    System.out.println("message offer: " + message.getPayload());
+//                }
+//                if (message.getPayload().contains("\"event\":\"answer\"")) {
+//                    System.out.println("message answer: " + message.getPayload());
+//                }
                 webSocketSession.sendMessage(message);
+                System.out.println("SEND MESSAGE: " + message.getPayload());
+                System.out.println("session.getId(): " + session.getId());
             }
         }
     }
@@ -27,5 +70,8 @@ public class SocketHandler extends TextWebSocketHandler {
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         sessions.add(session);
+        System.out.println("sessions.size(): " + sessions.size());
+        System.out.println("session.getId(): " + session.getId());
     }
+
 }
--- webrtc-text/src/main/resources/static/client.js
+++ webrtc-text/src/main/resources/static/client.js
@@ -3,27 +3,35 @@ var conn = new WebSocket('ws://localhost:8080/socket');
 
 conn.onopen = function() {
     console.log("Connected to the signaling server");
-    initialize();
+    // HEHU Initialize later by a button press
+    // initialize();
 };
 
 conn.onmessage = function(msg) {
     console.log("Got message", msg.data);
     var content = JSON.parse(msg.data);
     var data = content.data;
+    var fromName = content.fromName;
+    var toName = content.toName;
+    console.log("DATA", data);
+    console.log("fromName", fromName);
+    console.log("toName", toName);
+    console.log("nameInput.value", nameInput.value);
+
     switch (content.event) {
-        // when somebody wants to call us
-        case "offer":
-            handleOffer(data);
-            break;
-        case "answer":
-            handleAnswer(data);
-            break;
-        // when a remote peer sends an ice candidate to us
-        case "candidate":
-            handleCandidate(data);
-            break;
-        default:
-            break;
+    // when somebody wants to call us
+    case "offer":
+        handleOffer(data, fromName, nameInput.value);
+        break;
+    case "answer":
+        handleAnswer(data, fromName, toName);
+        break;
+    // when a remote peer sends an ice candidate to us
+    case "candidate":
+        handleCandidate(data, fromName, nameInput.value);
+        break;
+    default:
+        break;
     }
 };
 
@@ -31,98 +39,162 @@ function send(message) {
     conn.send(JSON.stringify(message));
 }
 
-var peerConnection;
-var dataChannel;
+var peerConnection = new Array();
+var dataChannel = new Array();
 var input = document.getElementById("messageInput");
+// HEHU New input to be able to tell the clients from each other
+var nameInput = document.getElementById("clientNameInput");
+var peerInput = document.getElementById("peerNamesInput");
+var peerArray = peerInput.value.split(',');
 
 function initialize() {
     var configuration = null;
 
-    peerConnection = new RTCPeerConnection(configuration, {
-        optional : [ {
-            RtpDataChannels : true
-        } ]
+    console.log(" ==== nameInput: " + nameInput.value);
+
+    console.log(" ==== peerArray: " + peerArray);
+    console.log(" ==== peerArray.length: " + peerArray.length);
+    console.log(" ==== peerArray[0]: " + peerArray[0]);
+
+    // Send a Ping to register to WebSocket handler
+    send({
+        event : "ping",
+        fromName: nameInput.value
     });
 
-    // Setup ice handling
-    peerConnection.onicecandidate = function(event) {
-        if (event.candidate) {
-            send({
-                event : "candidate",
-                data : event.candidate
-            });
-        }
-    };
+    for (index = 0; index < peerArray.length; index++) {
+        let myIndex=index;
 
-    // BEGIN: Added to the Baeldung example to make it work
-    // https://github.com/eugenp/tutorials/tree/master/webrtc
-    peerConnection.ondatachannel = function(event) {
-        var receiveChannel = event.channel;
-        receiveChannel.onmessage = function(event) {
-            console.log("peerConnection message:", event.data);
+        peerConnection[index] = new RTCPeerConnection(configuration, {
+            optional : [ {
+                RtpDataChannels : true
+            } ]
+        });
+
+        // Setup ice handling
+        peerConnection[index].onicecandidate = function(event) {
+            if (event.candidate) {
+                send({
+                    event : "candidate",
+                    data : event.candidate,
+                    fromName: nameInput.value,
+                    toName: peerArray[myIndex]
+                });
+            }
         };
-    };
-    // END: Added to the Baeldung example to make it work
 
-    // creating data channel
-    dataChannel = peerConnection.createDataChannel("dataChannel", {
-        reliable : true
-    });
+        // BEGIN: Added to the Baeldung example to make it work
+        // https://github.com/eugenp/tutorials/tree/master/webrtc
+        peerConnection[index].ondatachannel = function(event) {
+            var receiveChannel = event.channel;
+            receiveChannel.onmessage = function(event) {
+                console.log("peerConnection message:", event.data);
+            };
+        };
+        // END: Added to the Baeldung example to make it work
 
-    dataChannel.onerror = function(error) {
-        console.log("Error occured on datachannel:", error);
-    };
+        // creating data channel
+        dataChannel[index] = peerConnection[index].createDataChannel("dataChannel", {
+            reliable : true
+        });
 
-    // BEGIN: This can be removed as is has been replaced by the code above
-    // when we receive a message from the other peer, printing it on the console
-    // dataChannel.onmessage = function(event) {
-    //     console.log("message:", event.data);
-    // };
-    // END: This can be removed as is has been replaced by the code above
+        dataChannel[index].onerror = function(error) {
+            console.log("Error occured on datachannel:", error);
+        };
 
-    dataChannel.onclose = function() {
-        console.log("data channel is closed");
-    };
+        // BEGIN: This can be removed as is has been replaced by the code above
+        // when we receive a message from the other peer, printing it on the console
+        // dataChannel.onmessage = function(event) {
+        //     console.log("message:", event.data);
+        // };
+        // END: This can be removed as is has been replaced by the code above
+
+        dataChannel[index].onclose = function() {
+            console.log("data channel is closed");
+        };
+    }
 }
 
 function createOffer() {
-    peerConnection.createOffer(function(offer) {
-        send({
-            event : "offer",
-            data : offer
-        });
-        peerConnection.setLocalDescription(offer);
-    }, function(error) {
-        alert("Error creating an offer");
-    });
+    for (index = 0; index < peerArray.length; index++) {
+        var status = peerConnection[index].iceConnectionState;
+        console.log("peerConnection[" + index + "] status: " + status);
+
+        if (status === "new") {
+            // We need to declare these before being used in the
+            // function below.
+            let myToName = peerArray[index];
+            let myIndex = index;
+            peerConnection[index].createOffer(function(offer) {
+                send({
+                    event : "offer",
+                    data : offer,
+                    fromName: nameInput.value,
+                    toName: myToName
+                });
+                peerConnection[myIndex].setLocalDescription(offer);
+            }, function(error) {
+                alert("Error creating an offer");
+            });
+        }
+    }
 }
 
-function handleOffer(offer) {
-    peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
+function handleOffer(offer, fromName, toName) {
+    console.log("offer fromName: " + fromName);
+    console.log("offer toName: " + toName);
 
-    // create and send an answer to an offer
-    peerConnection.createAnswer(function(answer) {
-        peerConnection.setLocalDescription(answer);
-        send({
-            event : "answer",
-            data : answer
+    // Only answer offer if it's to you
+    if (toName === nameInput.value) {
+
+        peerIndex = peerArray.indexOf(fromName);
+        peerConnection[peerIndex].setRemoteDescription(new RTCSessionDescription(offer));
+        let myIndex = peerIndex;
+
+        // create and send an answer to an offer
+        peerConnection[peerIndex].createAnswer(function (answer) {
+            peerConnection[peerIndex].setLocalDescription(answer);
+            send({
+                event: "answer",
+                data: answer,
+                fromName: nameInput.value,
+                toName: peerArray[myIndex]
+            });
+        }, function (error) {
+            alert("Error creating an answer");
         });
-    }, function(error) {
-        alert("Error creating an answer");
-    });
-
+    }
 };
 
-function handleCandidate(candidate) {
-    peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
+
+function handleCandidate(candidate, fromName, toName) {
+    console.log("candidate fromName: " + fromName);
+    console.log("candidate toName: " + toName);
+
+    // FIXME problem
+    // Only answer candidate message if it's to you
+    if (toName === nameInput.value) {
+        peerIndex = peerArray.indexOf(fromName);
+        peerConnection[peerIndex].addIceCandidate(new RTCIceCandidate(candidate));
+    }
 };
 
-function handleAnswer(answer) {
-    peerConnection.setRemoteDescription(new RTCSessionDescription(answer));
+function handleAnswer(answer, fromName, toName) {
+    console.log("answer fromName: " + fromName);
+    console.log("answer toName: " + toName);
+
+    peerIndex = peerArray.indexOf(fromName);
+    peerConnection[peerIndex].setRemoteDescription(new RTCSessionDescription(answer));
     console.log("connection established successfully!!");
+
 };
 
 function sendMessage() {
-    dataChannel.send(input.value);
+    console.log("dataChannel list.length:", dataChannel.length);
+    console.log("dataChannel list:", dataChannel);
+
+    for (index = 0; index < dataChannel.length; index++) {
+        dataChannel[index].send(input.value);
+    }
     input.value = "";
 }
