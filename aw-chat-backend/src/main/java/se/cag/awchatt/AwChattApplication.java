8d5b05ebd62b5f2e2ca78aac98c481baf0acd9d7
Fredrik Dahlman <fredrik.dahlman@gmail.com>
2020-10-07 10:15:34 +0200

    Fixed CORS problem

--- aw-chat-backend/src/main/java/se/cag/awchatt/AwChattApplication.java
+++ aw-chat-backend/src/main/java/se/cag/awchatt/AwChattApplication.java
@@ -1,7 +1,9 @@
 package se.cag.awchatt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://localhost:8080/graphql")
 @SpringBootApplication
 public class AwChattApplication {
 
